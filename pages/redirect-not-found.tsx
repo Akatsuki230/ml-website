9187b90f2f0867173c3d8efd0d9f0d2fd4291e56
mldkyt <mldkyt@protonmail.com>
2023-03-03 11:27:57 +0000

    Add redirect not found page

---
+++ pages/redirect-not-found.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function RedirectNotFound() {
+    return (
+        <>
+            <Typography variant="h5">This is not existant.</Typography>
+        </>
+    )
+}
