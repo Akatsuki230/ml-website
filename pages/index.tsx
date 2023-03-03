0ccfd21140707a8649ee01d783d1e13dd6f36bb3
mldkyt <mldkyt@protonmail.com>
2023-03-03 19:48:48 +0000

    Add more content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import Head from 'next/head'
 import NavBar from '../components/NavBar'
-import {Button, Link, Stack, Typography, Alert} from "@mui/material";
-import {Inter} from '@next/font/google';
+import { Button, Link, Stack, Typography, Alert } from "@mui/material";
+import { Inter } from '@next/font/google';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { useRef } from 'react';
 import { PriorityHigh } from '@mui/icons-material';
 
-const inter = Inter({subsets: ['latin']});
+const inter = Inter({ subsets: ['latin'] });
 
 const Home = () => {
   const router = useRouter();
@@ -18,14 +18,14 @@ const Home = () => {
   useEffect(() => {
     if (!hasLoaded.current) {
       fetch('/api/visits')
-      .then(res => res.json())
-      .then(data => {
-        setVisits(data.visits);
-      });
+        .then(res => res.json())
+        .then(data => {
+          setVisits(data.visits);
+        });
       hasLoaded.current = true;
     }
   }, [])
-  
+
   return (
     <div className={inter.className}>
       <Head>
@@ -39,13 +39,54 @@ const Home = () => {
           <Button href="https://bot.mldkyt.com">See more</Button>
         </Alert>
         <Typography variant='body2'>{visits} visits</Typography>
-        <Typography variant='h4'>About MLDKYT</Typography>
-        <Typography variant='body1'>MLDKYT (mldkyt) is a YouTuber, programmer and a modder.</Typography>
-        <Typography variant='body1'>Most of his mods are for a game called <Link href='https://store.steampowered.com/app/516750/My_Summer_Car/'>My Summer Car</Link>.</Typography>  
         <Typography variant='h4'>My projects</Typography>
         <Button variant='contained' onClick={_ => router.push('/projects')}>View my projects</Button>
         <Typography variant='h4'>My social links</Typography>
         <Button onClick={_ => router.push('/social')} variant='contained'>Go to my social links</Button>
+        <Typography variant='h4'>About me</Typography>
+        <Typography>
+          Hello there! I'm a programmer with a passion for creating immersive and entertaining experiences in the gaming world. My expertise lies in modding games, and one of my current projects is modifying the popular game My Summer Car to enhance the user experience.
+        </Typography>
+        <br />
+        <Typography>
+          In addition to modding, I'm also developing my own game called IRL Simulator, which is a unique blend of simulation and role-playing. The game is still in development, but I'm excited to bring it to life and offer gamers a new and exciting way to play.
+        </Typography>
+        <Typography variant='h4'>My mods</Typography>
+        <Typography>
+          As a passionate game modder, I've created a variety of mods for the popular game My Summer Car. Each of these mods is designed to enhance the player experience and offer something new and exciting to the game.
+        </Typography>
+        <br />
+        <Typography>
+          One of my most popular mods is Asphalt roads, which replaces the standard dirt roads in the game with smooth, realistic asphalt roads. This mod not only adds a new level of realism to the game but also makes driving smoother and more enjoyable.
+        </Typography>
+        <br />
+        <Typography>
+          Another popular mod of mine is GoodOldMSC, which brings back the classic look and feel of the game from its earlier versions. This mod includes a variety of tweaks and changes to the game's graphics and interface, offering a nostalgic trip down memory lane for long-time fans of the game.
+        </Typography>
+        <br />
+        <Typography>
+          For players who enjoy driving the kekmet vehicle in My Summer Car, I've created several mods to enhance their experience. Kekmet speedometer and kekmet more gears offer new options for monitoring speed and shifting gears, respectively, making driving the kekmet even more fun and challenging.
+        </Typography>
+        <br />
+        <Typography>
+          If you're a fan of classic cars, my Old World and Old Highway Cars mods are perfect for you. Old World replaces the game's modern vehicles with classic cars from the 1960s and 70s, while Old Highway Cars adds a variety of vintage cars to the game's traffic.
+        </Typography>
+        <br />
+        <Typography>
+          For those who want to add a touch of nostalgia to their game, I've created Windows Sounds, which replaces the game's default sounds with classic Windows sound effects from previous versions of the operating system.
+        </Typography>
+        <br />
+        <Typography>
+          Finally, for fans of the game's iconic Hayosiko vehicle, my Old Hayosiko and OldCarSounds mods add new textures and sounds to the vehicle, making it feel more authentic and enjoyable to drive.
+        </Typography>
+        <br />
+        <Typography>
+          Overall, each of these mods is designed to offer something new and exciting to players of My Summer Car. Whether you're looking for improved graphics, new challenges, or a touch of nostalgia, my mods have you covered.
+        </Typography>
+        <br/>
+        <Typography variant="body2">
+        Please note that I am not a professional page designer, so I apologize if the aesthetics of this page are not up to par. My main focus is on creating engaging and informative content, rather than on page design. Nonetheless, I hope you find the information here helpful and enjoyable. Thank you for your understanding.
+        </Typography>
       </Stack>
     </div>
   )
