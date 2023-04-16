a57ec9b2b9b74f02c2d63400ebe1db483ec04b2b
mldkyt <mldkyt@proton.me>
2023-04-16 21:23:36 +0200

    Add google ads

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,16 @@ import type { AppProps } from 'next/app'
 import { Inter } from '@next/font/google'
 import { Analytics } from '@vercel/analytics/react';
 import Script from 'next/script';
+import Head from 'next/head';
 
 const inter = Inter({subsets: ['latin']});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4021488147419187" crossOrigin="anonymous"></script>
+      </Head>
       <div className={inter.className}>
         <Component {...pageProps} />
         <Analytics />
