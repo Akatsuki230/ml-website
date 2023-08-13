d45f6b12f24e94694e90754e07692012d1f8475a
mldkyt <mldkyt@protonmail.com>
2023-08-13 15:18:35 +0200

    test 1

--- src/pages/[redir].tsx
+++ src/pages/[redir].tsx
@@ -3,6 +3,8 @@ import {Simulate} from "react-dom/test-utils";
 import error = Simulate.error;
 import load = Simulate.load;
 import Head from "next/head";
+import {GetServerSidePropsContext} from "next";
+import {sql} from "@vercel/postgres";
 
 function renderRedirect(url: string, label: string, redirTime: number) {
     return (
@@ -46,58 +48,77 @@ function renderError(label: string) {
     )
 }
 
-export default function Redirection() {
-    const [loading, setLoading] = useState(true)
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+    const {redir} = context.params as { redir: string };
+    if (!redir) {
+        return {
+            props: {
+                error: true,
+                errorMessage: 'No redirect path provided.'
+            }
+        }
+    }
 
-    const [linkType, setLinkType] = useState('' as 'image' | 'redirect' | 'file' | 'error')
-    const [linkURL, setLinkURL] = useState("")
-    const [linkLabel, setLinkLabel] = useState("")
+    const data = await sql`SELECT *
+                           FROM redirects
+                           WHERE id = ${redir}`
+    if (data.rowCount === 0) {
+        return {
+            props: {
+                error: true,
+                errorMessage: 'No redirect found.'
+            }
+        }
+    }
 
+    return {
+        props: {
+            type: data.rows[0].type,
+            url: data.rows[0].url,
+            label: data.rows[0].label,
+            views: data.rows[0].views
+        }
+    }
+}
+
+type Data = {
+    type: 'redirect' | 'image' | 'file';
+    url: string;
+    label: string;
+    views: number;
+    error: boolean;
+    errorMessage: string;
+}
+
+export default function Redirection(props: Data) {
     const [redirectTime, setRedirectTime] = useState(3)
+    const [screenHeight, setScreenHeight] = useState(0)
 
     const hasRan = useRef(false)
-
-    const [screenHeight, setScreenHeight] = useState(0)
+    const countDown = useRef(3)
 
     function resizeEvent() {
         setScreenHeight(window.innerHeight)
     }
+
     useEffect(function () {
         window.addEventListener('resize', resizeEvent)
 
         if (!hasRan.current) {
             hasRan.current = true
-            setScreenHeight(window.innerHeight)
-            fetch(`/api/redirect/get?path=${document.location.pathname.substring(1)}`, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }).then(x => x.json()).then(x => {
-                setLoading(false)
-                if (x.error) {
-                    setLinkType('error')
-                    setLinkLabel(x.error)
-                }
-                else {
-                    setLinkType(x.type)
-                    setLinkURL(x.url)
-                    setLinkLabel(x.label)
-
-                    if (x.type == 'redirect') {
-                        let temp1 = redirectTime
-                        setInterval(function () {
-                            temp1 -= 1
-                            if (temp1 > 0) {
-                                setRedirectTime(temp1)
-                            }
-                            else {
-                                setRedirectTime(0)
-                                document.location.href = x.url
-                            }
-                        }, 1000)
+            resizeEvent()
+            if (props.type == 'redirect') {
+                setInterval(function () {
+                    if (countDown.current > 1) {
+                        countDown.current--
+                        setRedirectTime(countDown.current)
                     }
-                }
-            })
+                    else {
+                        setRedirectTime(0)
+                        document.location.href = props.url
+                    }
+                }, 1000)
+            }
         }
 
         return function () {
@@ -109,9 +130,43 @@ export default function Redirection() {
         <>
             <Head>
                 <title>Custom page</title>
-                <meta name="title" content="Custom page"/>
-                <meta name="description" content="This page can contain a redirect, image or download."/>
-                <meta name="theme-color" content="#00FFFF"/>
+                {
+                    props.type == 'redirect' && (
+                        <>
+                            <meta name="title" content="mldkyt has shared a link with you"/>
+                            <meta name="description" content={`Redirects to: ${props.label}`}/>
+                            <meta name="theme-color" content="#00FFFF"/>
+                        </>
+                    )
+                }
+                {
+                    props.type == 'image' && (
+                        <>
+                            <meta name="title" content="mldkyt has shared an image with you"/>
+                            <meta name="description" content={props.label}/>
+                            <meta name="theme-color" content="#00FFFF"/>
+                            <meta name="og:image" content={props.url}/>
+                        </>
+                    )
+                }
+                {
+                    props.type == 'file' && (
+                        <>
+                            <meta name="title" content="mldkyt has shared a file with you"/>
+                            <meta name="description" content={props.label}/>
+                            <meta name="theme-color" content="#00FFFF"/>
+                        </>
+                    )
+                }
+                {
+                    props.error && (
+                        <>
+                            <meta name="title" content="Error"/>
+                            <meta name="description" content={props.errorMessage}/>
+                            <meta name="theme-color" content="#F00"/>
+                        </>
+                    )
+                }
             </Head>
             <h1 className='text-2xl'>mldkyt has shared something with you:</h1>
             <br/>
@@ -119,16 +174,15 @@ export default function Redirection() {
             <div className='text-center absolute left-1/2 -translate-x-1/2 -translate-y-1/2' style={{
                 top: `${screenHeight / 2}px`
             }}>
-                {loading ? (
+                {props.error ? (
                     <>
-                        <h1 className='text-3xl'>Loading...</h1>
+                        {renderError(props.errorMessage)}
                     </>
                 ) : (
                     <>
-                        {linkType == 'redirect' && renderRedirect(linkURL, linkLabel, redirectTime)}
-                        {linkType == 'image' && renderImage(linkURL, linkLabel)}
-                        {linkType == 'file' && renderFile(linkURL, linkLabel)}
-                        {linkType == 'error' && renderError(linkLabel)}
+                        {props.type == 'redirect' && renderRedirect(props.url, props.label, redirectTime)}
+                        {props.type == 'image' && renderImage(props.url, props.label)}
+                        {props.type == 'file' && renderFile(props.url, props.label)}
                     </>
                 )}
             </div>
