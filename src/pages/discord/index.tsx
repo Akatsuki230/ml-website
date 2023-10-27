db87aeee24e767ef0725a28857f19a567db07872
Programmer Astolfo <programmerastolfo@gmail.com>
2023-10-27 20:26:13 +0200

    pluh

--- src/pages/discord/index.tsx
+++ src/pages/discord/index.tsx
@@ -96,68 +96,6 @@ export default function Discord() {
         <br />
         <ViewTracker />
       </Container>
-
-      <h1
-        className="mt-4 text-5xl text-center font-bold text-white mx-auto w-max"
-        style={{
-          background:
-            "linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red)",
-          WebkitBackgroundClip: "text",
-          color: "transparent",
-        }}
-      >
-        mldkyt's Discord server
-      </h1>
-      <div className="text-center">
-        <a href="https://discord.gg/JgFNmSwYME">
-          <button className="bg-blue-600 my-8 text-white px-4 py-2 rounded-md">
-            Join now
-          </button>
-        </a>
-        <br />
-        <a href="https://forms.gle/aXEzprRZxqs5Co86A" className="text-blue-600 underline">
-          Ban appeal
-        </a>
-        <br />
-      </div>
-      {loadedStats ? (
-        <>
-          <p className="text-white ml-8 text-xl">
-            <strong>{members}</strong> members
-          </p>
-          <p className="text-green-500 ml-8 text-xl">
-            <strong>{onlineMembers}</strong> online members
-          </p>
-        </>
-      ) : (
-        <>
-          <p
-            className="ml-8 text-xl"
-          >
-            Loading member stats...
-          </p>
-        </>
-      )}
-      <h2 className="text-3xl text-white px-4 mt-2 font-bold">
-        What does the Discord server offer?
-      </h2>
-      <p className="px-6 text-xl text-white font-bold">Custom Discord bot</p>
-      <p className="px-8 text-white">
-        The Discord has a custom bot which has ChatPoints, CutePoints and more.
-      </p>
-      <p className="px-8 text-white">
-        You can view the leaderboard for <a className="text-blue-600 underline" href="/discord/chatpoints">ChatPoints</a> and <a className="text-blue-600 underline" href="/discord/cutepoints">CutePoints</a> respectively.
-      </p>
-      <img
-        src="/Discord_LHPOzTvDUC.png"
-        alt="Discord bot"
-        className="mx-6 rounded-md mt-2"
-      />
-
-      <ViewTracker />
-      <Astolfo />
-      <FemboyFridayVirtualHeadpat />
-      <FinalNavbar sel="discord" />
     </div>
   );
 }
