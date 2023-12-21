30ee9716ae9af85eac6bc5d0a88dfbe767ccf9fb
mldkyt <mldkyt@proton.me>
2023-12-21 22:18:10 +0100

    Rebuild index page, remove ShareX settings and fembot updates

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,41 +6,51 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function FinalNavbar() {
     return (
-        <div className={inter.className}>
-            <Navbar>
-                <Container>
-                    <Navbar.Brand href="/">
-                        <Image src="/pfp.webp" width="30" height="30" className="d-inline-block align-top" style={{ borderRadius: "50%" }} alt="Astolfo's PFP" />
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                    <Navbar.Collapse>
-                        <Nav className="me-auto">
-                            <Nav.Link href="/meshsave">Meshsave</Nav.Link>
-                            <NavDropdown title="Projects">
-                                <small style={{marginLeft: '0.4em', color: 'grey', fontSize: '70%'}}>Online services</small>
-                                <NavDropdown.Item href="/article/newest">Newest articles</NavDropdown.Item>
-                                <NavDropdown.Item href="/fembot">FemBot</NavDropdown.Item>
-                                <small style={{marginLeft: '0.4em', color: 'grey', fontSize: '70%'}}>Smaller
-                                    projects</small>
-                                <NavDropdown.Item href="/project/astolfoiscomingforyou">Astolfo is Coming For
-                                    You</NavDropdown.Item>
-                                <NavDropdown.Item href="/project/astolfomod">Astolfo Forge Mod</NavDropdown.Item>
-                                <NavDropdown.Item href="/project/astolforesourcepack">Astolfo Resource
-                                    Pack</NavDropdown.Item>
-                                <NavDropdown.Item href="/project/astolfos">AstolfOS</NavDropdown.Item>
-                                <NavDropdown.Item href="/project/sharex">ShareX Settings</NavDropdown.Item>
-                                <NavDropdown.Item href="/femboy">Femboy List</NavDropdown.Item>
-                                <small style={{marginLeft: '0.4em', color: 'grey', fontSize: '70%'}}>Older</small>
-                                <NavDropdown.Item href="/project/olderprojects">Older Projects</NavDropdown.Item>
-                            </NavDropdown>
-                            <Nav.Link href="/social">Socials</Nav.Link>
-                            <Nav.Link href="/discord">Discord</Nav.Link>
-                            <Nav.Link href="/pronouns">Pronouns</Nav.Link>
-                            <Nav.Link href="/ideas">Send Ideas</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
+        <>
+            {/*trans flag*/}
+            <div style={{
+                height: '20px',
+                background: 'linear-gradient(to bottom, #5BCEFA 0% 20%, #F5A9B8 20% 40%, white 40% 60%, #F5A9B8 60% 80%, #5BCEFA 80% 100%)',
+            }}>
+
+            </div>
+            <div className={inter.className}>
+                <Navbar>
+                    <Container>
+                        <Navbar.Brand href="/">
+                            <Image src="/pfp.webp" width="30" height="30" className="d-inline-block align-top"
+                                   style={{borderRadius: "50%"}} alt="mldkyt's PFP"/>
+                        </Navbar.Brand>
+                        <Navbar.Toggle/>
+                        <Navbar.Collapse>
+                            <Nav className="me-auto">
+                                <Nav.Link href="/meshsave">Meshsave</Nav.Link>
+                                <NavDropdown title="Projects">
+                                    <small style={{marginLeft: '0.4em', color: 'grey', fontSize: '70%'}}>Online
+                                        services</small>
+                                    <NavDropdown.Item href="/article/newest">Newest articles</NavDropdown.Item>
+                                    <NavDropdown.Item href="/fembot">FemBot</NavDropdown.Item>
+                                    <small style={{marginLeft: '0.4em', color: 'grey', fontSize: '70%'}}>Smaller
+                                        projects</small>
+                                    <NavDropdown.Item href="/project/astolfoiscomingforyou">Astolfo is Coming For
+                                        You</NavDropdown.Item>
+                                    <NavDropdown.Item href="/project/astolfomod">Astolfo Forge Mod</NavDropdown.Item>
+                                    <NavDropdown.Item href="/project/astolforesourcepack">Astolfo Resource
+                                        Pack</NavDropdown.Item>
+                                    <NavDropdown.Item href="/project/astolfos">AstolfOS</NavDropdown.Item>
+                                    <NavDropdown.Item href="/femboy">Femboy List</NavDropdown.Item>
+                                    <small style={{marginLeft: '0.4em', color: 'grey', fontSize: '70%'}}>Older</small>
+                                    <NavDropdown.Item href="/project/olderprojects">Older Projects</NavDropdown.Item>
+                                </NavDropdown>
+                                <Nav.Link href="/social">Socials</Nav.Link>
+                                <Nav.Link href="/discord">Discord</Nav.Link>
+                                <Nav.Link href="/pronouns">Pronouns</Nav.Link>
+                                <Nav.Link href="/ideas">Send Ideas</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        </>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
-import { Inter } from "next/font/google";
+import {Inter} from "next/font/google";
 import FinalNavbar from "@/components/NavBar";
 import Head from "next/head";
-import { useEffect, useRef } from "react";
+import React, {useEffect, useRef} from "react";
 import FemboyFridayVirtualHeadpat from "@/components/FemboyFridayVirtualHeadpat";
-import { Alert, Button, Container } from "react-bootstrap";
-import ViewTracker from "@/components/ViewTracker";
-import LatestArticleDisplay from "@/components/LatestArticleDisplay";
+import {Button, Container, Image, ListGroup} from "react-bootstrap";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export default function Home() {
     const hasRan = useRef(false);
@@ -26,56 +24,90 @@ export default function Home() {
         <div className={inter.className}>
             <Head>
                 <title>mldkyt's website</title>
-                <meta name="description" content="Welcome to mldkyt's website!" />
-                <meta name="author" content="mldkyt" />
-                <meta name="theme-color" content="#FF77FF" />
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4021488147419187" crossOrigin="anonymous"></script>
+                <meta name="description" content="Welcome to mldkyt's website!"/>
+                <meta name="author" content="mldkyt"/>
+                <meta name="theme-color" content="#FF77FF"/>
+                <script async
+                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4021488147419187"
+                        crossOrigin="anonymous"></script>
             </Head>
 
-            <FinalNavbar />
+            <FinalNavbar/>
 
             <Container>
-                <FemboyFridayVirtualHeadpat />
-                <h1>mldkyt's website</h1>
-                <p>Welcome to mldkyt's website! Start scrolling down to find out more about me :3</p>
-                <ViewTracker />
-
-                <Alert variant="primary">
-                    <h4>FemBot - A Discord bot by mldkyt!</h4>
-                    <Button as="a" href="/fembot">
-                        See more info
-                    </Button>
-                </Alert>
-
-                <Alert variant="secondary">
-                    <h4>I have a Discord server! Hop in for some fun!</h4>
-                    <Button as="a" href="/discord">
-                        Join the Discord
-                    </Button>
-                </Alert>
-
-                <h1>About me</h1>
-                <p>mldkyt is a 🏳️‍⚧️ programmer who is very good at programming! &lt;3</p>
-                <h3>Preferred languages</h3>
-                <ul>
-                    <li>C#</li>
-                    <li>C++</li>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
-                    <li>Python</li>
-                    <li>Rust</li>
-                    <li>Go</li>
-                </ul>
-                <p>..and many more!</p>
-                <h2>Projects</h2>
-                <p>View my projects from the dropdown menu at the top of my page!</p>
-                <h1>Socials</h1>
-                <Button as="a" href="/social">Socials are listed here</Button>
-                <br/>
-                <br/>
-                <h1>Read some more</h1>
-                <LatestArticleDisplay />
+                <FemboyFridayVirtualHeadpat/>
+                <div>
+                    <Image src="/pfp.webp" width="100" height="100"
+                           style={{
+                               borderRadius: "50%",
+                               position: 'relative',
+                               left: '50%',
+                               transform: 'translateX(-50%)'
+                           }} alt="mldkyt's PFP"/>
+                    <h1 style={{
+                        fontSize: '48px',
+                        textAlign: "center"
+                    }}>mldkyt</h1>
+                </div>
+                <p style={{
+                    fontSize: '18px',
+                    fontWeight: 'bold'
+                }}>A transgender programmer from the Czech Republic.</p>
+                <p>Writes in a lot of languages, such as C#, C++, HTML, CSS, JavaScript, Python, Rust, Go, Batch, Bash,
+                    PHP, Lua (ComputerCraft), ...</p>
+                <h2>History of mldkyt</h2>
+                <p>Mldkyt's first appearance is on <a href="https://www.youtube.com/watch?v=pil1bYc7foI">YouTube</a>,
+                    dating back to 11/26/2019. She started posting content on YouTube primarily about My Summer Car. In
+                    2021, she expanded to making My Summer Car mods as well, alongside posting videos about existing
+                    mods.</p>
+                <p>Her first mod was <a href="https://www.nexusmods.com/mysummercar/mods/3666">posted on
+                    NexusMods <i>(reupload)</i></a> on 30th December 2020. The mod was coded so well that it's first
+                    version would work in the latest version of My Summer Car. She has updated this mod many times,
+                    adding more features to the mod, such as: Assembly sounds, sound improvements, old dashboard,
+                    disable door sounds, walking sounds and knob sounds, just like in the old versions of My Summer Car.
+                </p>
+                <p>Her most successful mod as of now is Asphalt roads, getting over 10k downloads within the first week
+                    and having over 30k downloads till this day. The mod had one update, improving the behaviour of the
+                    roads, as well as adding the ability to convert more roads to asphalt.</p>
+                <h2>Currently active projects</h2>
+                <ListGroup>
+                    <ListGroup.Item>
+                        <h3>FemBot</h3>
+                        <p>A Discord bot made by mldkyt.</p>
+                        <Button as="a" href="/fembot">Invite, more information and feature suggestion here!</Button>
+                    </ListGroup.Item>
+                </ListGroup>
+                <h2>Inactive projects (supported, not actively updated)</h2>
+                <ListGroup>
+                    <ListGroup.Item>
+                        <h3>Astolfo is coming for you</h3>
+                        <p>A program where Astolfo comes for you. Good and evil versions are available.</p>
+                        <Button as="a" href="/project/astolfoiscomingforyou">Download here!</Button>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <h3>Astolfo Forge Mod</h3>
+                        <p>A Minecraft mod that adds Astolfo, Felix, Rimuru, Hideri, Bridget and many more into the
+                            game.</p>
+                        <Button as="a" href="/project/astolfomod">Download here!</Button>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <h3>Astolfo Resource Pack</h3>
+                        <p>A resource pack that changes a few things to Astolfo.</p>
+                        <Button as="a" href="/project/astolforesourcepack">Download here!</Button>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <h3>AstolfOS</h3>
+                        <p>An operating system which has Astolfo in a lot of places.</p>
+                        <Button as="a" href="/project/astolfos">Download here!</Button>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <h3>Femboy List</h3>
+                        <p>A femboy list.</p>
+                        <Button as="a" href="/femboy">View it here</Button>
+                    </ListGroup.Item>
+                </ListGroup>
+                <h2>Unmaintained projects</h2>
+                <Button as="a" href="/project/olderprojects">View unmaintained projects</Button>
             </Container>
         </div>
     );
