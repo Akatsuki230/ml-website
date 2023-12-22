c1ed1eef46c888f73a5f7c5d52fc9d71941af616
mldkyt <mldkyt@proton.me>
2023-12-22 21:54:33 +0100

    Discord page redesign, fix ideas page, fix bug

--- src/pages/api/article/latest.ts
+++ src/pages/api/article/latest.ts
@@ -1,17 +1,14 @@
-import {NextApiRequest, NextApiResponse} from "next";
-import {ArticleBase, ArticleExtended} from "@/types/articles";
-
-
+import { NextApiRequest, NextApiResponse } from "next";
+import { ArticleBase, ArticleExtended } from "@/types/articles";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const response = await fetch(`${process.env.FIREBASE_URL}/articles.json`);
-    const articles = await response.json() as ArticleBase[];
-    const articlesWithTimestamps = articles
-        .map((x: ArticleExtended) => {
-            x.timeOrdering = (x.postYear - 2020) * 365 + x.postMonth * 28 + x.postDay;
-            return x;
-        })
-        .sort((a, b) => a.timeOrdering - b.timeOrdering);
+    const articles = (await response.json()) as ArticleBase[];
+    const articlesWithTimestamps = articles.map((x: ArticleExtended) => {
+        x.timeOrdering = (x.postYear - 2020) * 365 + x.postMonth * 28 + x.postDay;
+        return x;
+    });
+    articlesWithTimestamps.sort((a, b) => a.timeOrdering - b.timeOrdering);
 
     let takeOnly25 = [] as ArticleExtended[];
     for (let i = 0; i < Math.min(articlesWithTimestamps.length, 25); i++) {
--- src/pages/discord/index.tsx
+++ src/pages/discord/index.tsx
@@ -3,8 +3,8 @@ import FinalNavbar from "@/components/NavBar";
 import ViewTracker from "@/components/ViewTracker";
 import { Inter } from "next/font/google";
 import Head from "next/head";
-import React from "react";
-import { Alert, Button, Container, Image, Modal } from "react-bootstrap";
+import React, { useState } from "react";
+import { Alert, Button, Container, FormControl, Image, Modal } from "react-bootstrap";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +13,14 @@ export default function Discord() {
     const [onlineMembers, setOnlineMembers] = React.useState(0);
     const [loadedStats, setLoadedStats] = React.useState(false);
     const [statsError, setStatsError] = React.useState(false);
+
+    const [bannedDialog, setBannedDialog] = useState(false);
+    const [bannedUsername, setBannedUsername] = useState("");
+    const [bannedBanned, setBannedBanned] = useState(false);
+    const [bannedLoading, setBannedLoading] = useState(false);
+    const [bannedReason, setBannedReason] = useState("");
+    const [bannedRan, setBannedRan] = useState(false);
+
     const hasRan = React.useRef(false);
 
     const [joinWarningShown, setJoinWarningShown] = React.useState(false);
@@ -40,6 +48,27 @@ export default function Discord() {
         fetchStats();
     }, [setMembers, setOnlineMembers, setLoadedStats, hasRan]);
 
+    function validateBanReason() {
+        setBannedLoading(true);
+        setBannedRan(false);
+        fetch(`/api/discord/ban?username=${bannedUsername}`).then((x) => {
+            if (!x.ok) {
+                alert("Something went wrong.");
+                setBannedLoading(false);
+                return;
+            }
+
+            x.json().then((y: { banned: boolean; reason: string | undefined }) => {
+                setBannedRan(true);
+                setBannedBanned(y.banned);
+                setBannedLoading(false);
+                if (y.banned && y.reason) {
+                    setBannedReason(y.reason);
+                }
+            });
+        });
+    }
+
     return (
         <div className={inter.className}>
             <Head>
@@ -51,118 +80,115 @@ export default function Discord() {
             </Head>
             <FinalNavbar />
             <Container>
-                <FemboyFridayVirtualHeadpat/>
-                <h1>mldkyt's Discord server</h1>
-                <p>The official Discord server for mldkyt, named "mldkyt's bedroom"</p>
-                <Button onClick={() => setJoinWarningShown(true)}>Join now</Button>
-                <Button style={{marginLeft: "15px"}} as="a" href="https://bit.ly/411oEUV" variant="secondary">
-                    I'm banned
-                </Button>
-
-                <br/>
-                <br/>
-                {loadedStats ? (
-                    <p>
-                        <strong>{members}</strong> members,
-                        <br/>
-                        <strong style={{color: "lime"}}>{onlineMembers}</strong> online members
-                    </p>
-                ) : (
-                    <p>{statsError ? <>There was an error loading the stats</> : <>Loading member stats...</>}</p>
-                )}
-
-                <h2>Features of this Discord server</h2>
-                <h3>Custom Discord Bot</h3>
-
-                <p>This server has a custom Discord bot which has ChatPoints, CutePoints and more.</p>
-                <p>
-                    You can view the leaderboard for{" "}
-                    <a className="text-blue-600 underline" href="/discord/chatpoints">
-                        ChatPoints
-                    </a>{" "}
-                    and{" "}
-                    <a className="text-blue-600 underline" href="/discord/cutepoints">
-                        CutePoints
-                    </a>{" "}
-                    respectively.
-                </p>
-                <Image style={{maxWidth: "100%"}} src="/discord_bot.webp" alt="Discord bot"/>
-
-                <h3>Only the best channels</h3>
-
-                <h4>#OwO</h4>
-                <p>A channel for only "owo" and "uwu" messages.</p>
-                <Image style={{maxWidth: "100%"}} src="/owo_channel_list.webp" alt="OwO channel in channel list"/>
-                <br/>
-                <br/>
-                <Image style={{maxWidth: "100%"}} src="/owo_channel_2.webp" alt="OwO channel"/>
-
-                <br/>
-                <br/>
-                <h2>#nya</h2>
-                <p>A channel for only meowing.</p>
-                <Image style={{maxWidth: "100%"}} src="/nya_channel_list.webp" alt="Nya channel in channel list"/>
-                <br/>
-                <br/>
-                <Image style={{maxWidth: "100%"}} src="/nya_channel_2.webp" alt="Nya channel"/>
-                <h3>Discord Bot</h3>
-                <div style={{marginLeft: "8px"}}>
-                    <p>A Discord bot that does many things.</p>
-                    <p>It's currently in development, but it's still usable.</p>
-                    <Button as="a" href="/discord">
-                        Learn more
+                <FemboyFridayVirtualHeadpat />
+                <div
+                    style={{
+                        textAlign: "center"
+                    }}
+                >
+                    <h1>mldkyt's Discord server</h1>
+                    {loadedStats ? (
+                        <p>
+                            <strong>{members}</strong> members,
+                            <strong style={{ color: "lime", marginLeft: "1ch" }}>{onlineMembers}</strong> online members
+                        </p>
+                    ) : (
+                        <p>{statsError ? <>There was an error loading the stats</> : <>Loading member stats...</>}</p>
+                    )}
+                    <Button onClick={() => setJoinWarningShown(true)}>Join now</Button>
+                    <Button style={{ marginLeft: "15px" }} onClick={() => setBannedDialog(true)} variant="secondary">
+                        I'm banned
                     </Button>
-                    <br/>
-                    <br/>
                 </div>
-                <br/>
-                <br/>
-                <h4>#:3</h4>
-                <p>A channel for only ":3" messages.</p>
-                <Image style={{maxWidth: "100%"}} src="/3_channel_list.webp" alt=":3 channel in channel list"/>
-                <br/>
-                <br/>
-                <Image style={{maxWidth: "100%"}} src="/3_channel_2.webp" alt=":3 channel"/>
 
-                <h3>Quality Announcemnets</h3>
+                <h2>Features of the server</h2>
+                <h3>A big selection of channels</h3>
+                <p>There is a big selection of channels in the server.</p>
+                <Image
+                    src="/discord/discord_channels.webp"
+                    style={{
+                        maxWidth: "100%"
+                    }}
+                />
 
-                <p>We have amazing admins and mods which make amazing announcements.</p>
-                <Image style={{maxWidth: "100%"}} src="/announcement_1.webp" alt="Quality announcement"/>
-                <p>
-                    <small>This is just an example.</small>
-                </p>
+                <h3>Active community</h3>
+                <p>The community is active, getting over 2000 messages a day.</p>
+                <Image
+                    src="/discord/discord_activity.webp"
+                    style={{
+                        maxWidth: "100%"
+                    }}
+                />
 
                 <Modal show={joinWarningShown} onHide={() => setJoinWarningShown(false)}>
-                    <Modal.Dialog>
-                        <Modal.Header closeButton>
-                            <Modal.Title>Before joining the server</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <Alert variant="warning">
-                                <p>
-                                    This server is <strong>NOT</strong> for people who are:
-                                </p>
-                                <ul>
-                                    <li>Homophobic</li>
-                                    <li>Transphobic</li>
-                                    <li>Femboyphobic</li>
-                                </ul>
-                                <p>This server is meant to be a welcoming place for everyone.</p>
-                            </Alert>
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button as="a" href="https://discord.gg/JgFNmSwYME">
-                                OK, Join
-                            </Button>
-                            <Button variant="secondary" onClick={() => setJoinWarningShown(false)}>
-                                Nevermind
-                            </Button>
-                        </Modal.Footer>
-                    </Modal.Dialog>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Before joining the server</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Alert variant="warning">
+                            <p>
+                                This server is <strong>NOT</strong> for people who are:
+                            </p>
+                            <ul>
+                                <li>Homophobic</li>
+                                <li>Transphobic</li>
+                            </ul>
+                            <p>This server is meant to be a welcoming place for everyone.</p>
+                        </Alert>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button as="a" href="https://discord.gg/JgFNmSwYME">
+                            OK, Join
+                        </Button>
+                        <Button variant="secondary" onClick={() => setJoinWarningShown(false)}>
+                            Nevermind
+                        </Button>
+                    </Modal.Footer>
                 </Modal>
 
-                <br/>
-                <ViewTracker/>
+                <Modal show={bannedDialog} onHide={() => setBannedDialog(false)}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Check your ban status</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <p>Here you can check your ban status.</p>
+                        <p>Simply input your Discord username below and the system will check your ban reason.</p>
+                        <FormControl type="text" value={bannedUsername} onChange={(x) => setBannedUsername(x.target.value)} disabled={bannedLoading}></FormControl>
+                        <br />
+                        <Button onClick={validateBanReason} disabled={bannedLoading}>
+                            Validate
+                        </Button>
+                        {bannedRan && (
+                            <>
+                                <br />
+                                <br />
+                                {bannedBanned ? (
+                                    <>
+                                        <Alert variant="danger">
+                                            <p>
+                                                <strong>You are banned.</strong>
+                                            </p>
+                                            <span>Reason: {bannedReason}</span>
+                                        </Alert>
+                                        <Button as="a" href="https://bit.ly/411oEUV">
+                                            Appeal ban
+                                        </Button>
+                                    </>
+                                ) : (
+                                    <Alert variant="success">
+                                        <span>
+                                            <strong>You are not banned!</strong>
+                                        </span>
+                                    </Alert>
+                                )}
+                            </>
+                        )}
+                    </Modal.Body>
+                </Modal>
+
+                <br />
+                <br />
+                <ViewTracker />
             </Container>
         </div>
     );
