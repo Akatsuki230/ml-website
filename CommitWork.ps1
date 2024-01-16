5c7b7181a9a852602aecfa725b2b6509f7b4a7ba
mldkyt <mldkyt@proton.me>
2024-01-16 18:46:11 +0100

    test

--- CommitWork.ps1
+++ CommitWork.ps1
@@ -1,26 +1,26 @@
 $msg = Read-Host "Enter commit message"
 
-Write-Progress -Activity "Committing" -Status "Committing changes to git" -CurrentOperation "git add ." -PercentComplete 0
+Write-Progress -Activity "Committing" -Status "git add ."  -PercentComplete 0
 git add . | Out-Null
-Write-Progress -Activity "Committing" -Status "Committing changes to git" -CurrentOperation "git commit -m $msg" -PercentComplete 10
+Write-Progress -Activity "Committing" -Status "git commit -m $msg"  -PercentComplete 10
 git commit -m $msg | Out-Null
-Write-Progress -Activity "Committing" -Status "Committing changes to git" -CurrentOperation "git push --set-upstream origin staging" -PercentComplete 20
+Write-Progress -Activity "Committing" -Status "git push --set-upstream origin staging"  -PercentComplete 20
 git push --set-upstream origin staging | Out-Null
-Write-Progress -Activity "Committing" -Status "Committing changes to git" -CurrentOperation "git push --set-upstream gitlab staging"  -PercentComplete 30
+Write-Progress -Activity "Committing" -Status "git push --set-upstream gitlab staging"   -PercentComplete 30
 git push --set-upstream gitlab staging | Out-Null
-Write-Progress -Activity "Committing" -Status "Committing changes to git" -CurrentOperation "git branch -M master"  -PercentComplete 40
+Write-Progress -Activity "Committing" -Status "git branch -M master"   -PercentComplete 40
 git branch -M master | Out-Null
-Write-Progress -Activity "Committing" -Status "Committing changes to git" -CurrentOperation "git merge gitlab/staging"  -PercentComplete 50
+Write-Progress -Activity "Committing" -Status "git merge gitlab/staging"   -PercentComplete 50
 git merge gitlab/staging | Out-Null
-Write-Progress -Activity "Committing" -Status "Committing changes to git" -CurrentOperation "git push --set-upstream origin master" -PercentComplete 60
+Write-Progress -Activity "Committing" -Status "git push --set-upstream origin master"  -PercentComplete 60
 git push --set-upstream origin master | Out-Null
-Write-Progress -Activity "Committing" -Status "Committing changes to git" -CurrentOperation "git push --set-upstream gitlab master"  -PercentComplete 70
+Write-Progress -Activity "Committing" -Status "git push --set-upstream gitlab master"   -PercentComplete 70
 git push --set-upstream gitlab master | Out-Null
-Write-Progress -Activity "Committing" -Status "Committing changes to git" -CurrentOperation "git branch -M staging" -PercentComplete 80
+Write-Progress -Activity "Committing" -Status "git branch -M staging"  -PercentComplete 80
 git branch -M staging | Out-Null
-Write-Progress -Activity "Committing" -Status "Committing changes to git" -CurrentOperation "git fetch --all" -PercentComplete 90
+Write-Progress -Activity "Committing" -Status "git fetch --all"  -PercentComplete 90
 git fetch --all | Out-Null
-Write-Progress -Activity "Committing" -Status "Committing changes to git" -PercentComplete 100
+Write-Progress -Activity "Committing" -Status "Done" -PercentComplete 100
 
 Write-Progress -Activity "Committing" -Completed
 
